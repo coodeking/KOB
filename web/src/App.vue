5b92f33748bf54ce9ecdfa0b86b2b4b938a1bdc1
2304755125@qq.com <2304755125@qq.com>
2023-09-17 14:11:39 +0800

    前端项目已创建

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div>AI昵称:{{AI_name}}</div>
+    <div>AI战力:{{AI_rating}}</div>  
+  </div>
+  <router-view></router-view>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body{
+  background-image: url("@/assets/background2.png");
+  background-size: cover;
 }
 </style>
+
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+ 
+export default {
+  name:"APP",
+  setup:() => {
+    let AI_name = ref("");
+    let AI_rating = ref("");
+
+    $.ajax({
+      url:"http://127.0.0.1:3000/pk/getbotinfo/",
+      type:"get",
+      success: resp => {
+        AI_name.value = resp.name,
+        AI_rating.value = resp.rating
+      } 
+    })
+
+    return {
+      AI_name,
+      AI_rating
+    }
+  }
+}
+</script>
